799b150cddd96c5e1e2ccb61bdce50e4532e097d
unknown <saadelsabahy11@gmail.com>
2020-08-30 18:28:02 +0200

    fix refresh

--- App.js
+++ App.js
@@ -1,42 +1,42 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import List from './src/components/List';
-import {ApolloProvider, InMemoryCache, HttpLink} from '@apollo/client';
-import {ApolloOfflineProvider} from 'react-offix-hooks';
-import {offlineClient} from './src/components/offix';
+import { ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloOfflineProvider } from 'react-offix-hooks';
+import { offlineClient } from './src/components/offix';
 const App = () => {
-  const cache = new InMemoryCache({});
-  const link = new HttpLink({uri: 'https://api.github.com/graphql'});
-  const [initialized, setInitialized] = useState(false);
+   const cache = new InMemoryCache({});
+   const link = new HttpLink({ uri: 'https://api.github.com/graphql' });
+   const [initialized, setInitialized] = useState(false);
 
-  // initialize the offix client and set the apollo client
-  useEffect(() => {
-    offlineClient.init().then(() => setInitialized(true));
-  }, []);
+   // initialize the offix client and set the apollo client
+   useEffect(() => {
+      offlineClient.init().then(() => setInitialized(true));
+      console.log(offlineClient.queue);
+   }, []);
 
-  if (!initialized) {
-    return (
-      <View style={styles.container}>
-        <Text>loading..</Text>
-      </View>
-    );
-  }
-  return (
-    <ApolloOfflineProvider client={offlineClient}>
-      <ApolloProvider client={offlineClient}>
-        <View style={styles.container}>
-          <List />
-        </View>
-      </ApolloProvider>
-    </ApolloOfflineProvider>
-  );
+   return (
+      <ApolloOfflineProvider client={offlineClient}>
+         <ApolloProvider client={offlineClient}>
+            <View style={styles.container}>
+               {!initialized ? (
+                  <Text style={{ alignSelf: 'center', fontSize: 18 }}>
+                     loading..
+                  </Text>
+               ) : (
+                  <List />
+               )}
+            </View>
+         </ApolloProvider>
+      </ApolloOfflineProvider>
+   );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignContent: 'center',
+   },
 });
 
 export default App;
--- src/components/offix.js
+++ src/components/offix.js
@@ -1,64 +1,65 @@
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import {HttpLink} from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import AsyncStorage from '@react-native-community/async-storage';
 // import NetInfo from "@react-native-community/netinfo"
-import {ApolloOfflineClient} from 'offix-client';
-import {ReactNativeNetworkStatus} from './NetworkStatus';
-import {CacheOperation, getUpdateFunction} from 'offix-cache';
-import {GET_REPOS} from './List';
+import { ApolloOfflineClient } from 'offix-client';
+import { ReactNativeNetworkStatus } from './NetworkStatus';
+import { CacheOperation, getUpdateFunction } from 'offix-cache';
+import { GET_REPOS } from './List';
 const cacheStorage = {
-  getItem: async (key) => {
-    const data = await AsyncStorage.getItem(key);
-    if (typeof data === 'string') {
-      console.log('Get item string', data);
-      return JSON.parse(data);
-    }
-    console.log('Get item', data);
-    return data;
-  },
-  setItem: (key, value) => {
-    let valueStr = value;
-    if (typeof valueStr === 'object') {
-      valueStr = JSON.stringify(value);
-    }
-    console.log('set item', valueStr);
-    return AsyncStorage.setItem(key, valueStr);
-  },
-  removeItem: (key) => {
-    return AsyncStorage.removeItem(key);
-  },
+   getItem: async (key) => {
+      const data = await AsyncStorage.getItem(key);
+      if (typeof data === 'string') {
+         console.log('Get item string', data);
+         return JSON.parse(data);
+      }
+      console.log('Get item', data);
+      return data;
+   },
+   setItem: (key, value) => {
+      let valueStr = value;
+      if (typeof valueStr === 'object') {
+         valueStr = JSON.stringify(value);
+      }
+      console.log('set item', valueStr);
+      return AsyncStorage.setItem(key, valueStr);
+   },
+   removeItem: (key) => {
+      return AsyncStorage.removeItem(key);
+   },
 };
 
 const networkStatus = new ReactNativeNetworkStatus();
 const options = {
-  updateQuery: GET_REPOS,
-  returnType: 'Repo',
+   updateQuery: GET_REPOS,
+   returnType: 'Repo',
 };
-export const reposUpdate = {
-  addStar: getUpdateFunction({
-    mutationName: 'addStar',
-    idField: 'id',
-    operationType: CacheOperation.ADD,
-    ...options,
-  }),
-  removeStar: getUpdateFunction({
-    mutationName: 'removeStar',
-    idField: 'id',
-    operationType: CacheOperation.ADD,
-    ...options,
-  }),
-};
+/* export const reposUpdate = {
+   addStar: getUpdateFunction({
+      mutationName: 'addStar',
+      idField: 'id',
+      operationType: CacheOperation.ADD,
+      ...options,
+   }),
+   removeStar: getUpdateFunction({
+      mutationName: 'removeStar',
+      idField: 'id',
+      operationType: CacheOperation.ADD,
+      ...options,
+   }),
+
+}; */
 export const offlineClient = new ApolloOfflineClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'https://api.github.com/graphql',
-    headers: {
-      Authorization: 'Bearer  20fb9b4b7933e9141bd302631b30b9ff690d19a2',
-    },
-  }),
-  offlineStorage: cacheStorage,
-  cacheStorage,
-  networkStatus,
-  mutationCacheUpdates: reposUpdate,
-  retryOptions: {attempts: {max: 100}, delay: {initial: 1000}},
+   cache: new InMemoryCache(),
+   link: new HttpLink({
+      uri: 'https://api.github.com/graphql',
+      headers: {
+         Authorization: 'Bearer  20fb9b4b7933e9141bd302631b30b9ff690d19a2',
+      },
+   }),
+   offlineStorage: cacheStorage,
+   cacheStorage,
+   networkStatus,
+   //  mutationCacheUpdates: reposUpdate,
+   retryOptions: { attempts: { max: 100 }, delay: { initial: 1000 } },
 });
